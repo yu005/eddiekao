aa1f0c202bc589b6e8b1243dad68319b9ce1605e
yu005 <146842778+yu005@users.noreply.github.com>
2023-11-14 20:50:00 +0800

    Add files via upload

---
+++ 1114/1114.js
@@ -0,0 +1,31 @@
+
+$(document).ready(function () {
+    $(".changeToOne").click(function(){
+        $("#exam").text(1);
+    })
+    $(".changeToTwo").click(function(){
+    $("#exam").text(2);
+});
+    
+    $(".hide").click(function () {
+    $(".main").slideUp(300);
+    //main類別的全部物件會有動畫出現
+    });
+    
+    $(".show").click(function () {
+    $(".main").slideDown(300);
+    });
+
+    $("#addBtn").click(function (e){
+        $("ol").append("<li>新增的</li><button>刪除</button>");
+    });
+
+    $("#removeBtn").click(function(){
+        $("li:last-child").remove();
+    });
+
+    $("#prependBtn").click(function(e){
+        $("#prependTest").prepend("新增的");
+    });
+});
+
